650c46717b03336e0de04aa5e11fa9d5c3fff314
Lucia Cotrozzi <luciacotrozzi@macbook-pro-di-lucia.local>
2018-02-09 10:28:32 +0100

    Onboarding

---
+++ The Drifters/Onboarding/PageViewController.swift
@@ -0,0 +1,98 @@
+//
+//  PageViewController.swift
+//  The Drifters
+//
+//  Created by Lucia Cotrozzi on 09/02/2018.
+//  Copyright © 2018 Graziella Caputo. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newVc(viewController: "Welcome"), self.newVc(viewController: "Garden"), self.newVc(viewController: "Recognition")]
+    } ()
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        let previousIndex = viewControllerIndex - 1
+        
+        print(previousIndex)
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        let nextIndex = viewControllerIndex + 1
+        
+        
+        
+        guard orderedViewControllers.count != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    var pageControl = UIPageControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = self
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+        
+        self.delegate = self
+        configurePageControl()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func configurePageControl() {
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: UIScreen.main.bounds.maxY - 50, width: UIScreen.main.bounds.width, height: 50))
+        
+        pageControl.numberOfPages = orderedViewControllers.count
+        pageControl.currentPage = 0
+        pageControl.tintColor = UIColor(red:2.55, green:0.59, blue:0.48, alpha:1.0)
+        //Not Selected
+        pageControl.pageIndicatorTintColor = UIColor(red:2.55, green:0.59, blue:0.48, alpha:1.0)
+        //Selected
+        pageControl.currentPageIndicatorTintColor = UIColor(red:1.89, green:0.39, blue:0.30, alpha:1.0)
+        self.view.addSubview(pageControl)
+    }
+    
+    func newVc(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Onboarding", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
+
+}
