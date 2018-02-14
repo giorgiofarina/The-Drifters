d74237e98671fb21b80fedc018062849c910965e
Graziella Caputo <caputograziella7@gmail.com>
2018-02-14 15:26:37 +0100

    new Search View

--- The Drifters/Filters/SearchByFiltersViewController.swift
+++ The Drifters/Filters/SearchByFiltersViewController.swift
@@ -9,98 +9,110 @@
 import UIKit
 import CoreLocation
 
-class SearchByFiltersViewController:  UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, CLLocationManagerDelegate, UITextFieldDelegate, UISearchBarDelegate {
-    
-    
-    
+class SearchByFiltersViewController:  UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate  {
+
         
         @IBOutlet weak var scrollView: UIScrollView!
         
-        @IBOutlet weak var currentLocationButton: UIButton!
+   
 
         @IBOutlet weak var matchingButton: UIButton!
     
+        @IBOutlet weak var climateButton: UIButton!
         @IBOutlet weak var dedicationButton: UIButton!
+        @IBOutlet weak var plantSizeButton: UIButton!
         @IBOutlet weak var categoryButton: UIButton!
         @IBOutlet weak var spaceButton: UIButton!
         @IBOutlet weak var brightnessButton: UIButton!
     
-        @IBOutlet weak var categoryTextField: UITextField!{
+    
+    
+    
+        @IBOutlet weak var climateTextField: UITextField!{
             didSet {
-                categoryTextField.delegate = self
+                climateTextField.delegate = self
             }
         }
+        @IBOutlet weak var brightnessTextField: UITextField!{
+            didSet {
+                brightnessTextField.delegate = self
+            }
+        }
+        @IBOutlet weak var dedicationTextField: UITextField!{
+            didSet {
+                dedicationTextField.delegate = self
+            }
+        }
+        @IBOutlet weak var plantSizeTextField: UITextField!{
+                didSet {
+                    plantSizeTextField.delegate = self
+                }
+            }
+        @IBOutlet weak var categoryTextField: UITextField!{
+                didSet {
+                    categoryTextField.delegate = self
+                }
+            }
         @IBOutlet weak var spaceTextField: UITextField!{
             didSet {
                 spaceTextField.delegate = self
             }
         }
-    
-    
-    
-    
-        @IBOutlet weak var dedicationTextField: UITextField!
-        @IBOutlet weak var brightnessTextField: UITextField!
-    
-        @IBOutlet weak var searchBar: UISearchBar!
-    
-    
+ 
 
     
-    
+        let climatePickerView = UIPickerView()
+        let brightnessPickerView = UIPickerView()
+        let dedicationPickerView = UIPickerView()
+        let plantSizePickerView = UIPickerView()
         let categoryPickerView = UIPickerView()
         let spacePickerView = UIPickerView()
-        let dedicationPickerView = UIPickerView()
     
     
+    
+        let climate = ["Mediterranean", "Tropical", "Polar", "Dry",             "Moderate", "Continental"]
+        let brightness = ["High", "Medium", "Low"]
+        let dedication = ["5min per day",
+                          "15min per day",
+                          "1h per day",
+                          "more than 1h per day", "1h per week"]
+        let plantSize = ["Big",
+                          "Medium",
+                          "Small"]
         let category = ["Officinal Plants",
                     "Ornamental Plants",
                     "Aquatic Plants",
                     "Vegetables", "Fruit Plants"]
         let space = ["Inside","Outside"]
     
-        let dedication = ["5min per day",
-                      "15min per day",
-                      "1h per day",
-                      "more than 1h per day", "1h per week"]
-    
-    
-        var latitude: String = ""
-        var longitude: String = ""
-        
-        var locationManager: CLLocationManager!
-        var location : CLLocation! {
-            didSet{
-                latitude = "\(location.coordinate.latitude)"
-                longitude = "\(location.coordinate.longitude)"
-                
-            }
-            
-        }
+
         
         
         override func viewDidLoad() {
             super.viewDidLoad()
             self.navigationController?.isNavigationBarHidden = true
             
-            scrollView.isScrollEnabled = false
-            scrollView.contentInsetAdjustmentBehavior = .never
+            scrollView.isScrollEnabled = true
+//            scrollView.contentInsetAdjustmentBehavior = .never
+            
             
     //SETTING SHAPES OF THE BUTTON
+
+            
+            climateButton.layer.cornerRadius = 10
+            climateButton.layer.masksToBounds = true
+            brightnessButton.layer.cornerRadius = 10
+            brightnessButton.layer.masksToBounds = true
+            dedicationButton.layer.cornerRadius = 10
+            dedicationButton.layer.masksToBounds = true
+            plantSizeButton.layer.cornerRadius = 10
+            plantSizeButton.layer.masksToBounds = true
             categoryButton.layer.cornerRadius = 10
             categoryButton.layer.masksToBounds = true
             spaceButton.layer.cornerRadius = 10
             spaceButton.layer.masksToBounds = true
-            dedicationButton.layer.cornerRadius = 10
-            dedicationButton.layer.masksToBounds = true
-            matchingButton.layer.cornerRadius = 10
-            matchingButton.layer.masksToBounds = true
-            brightnessButton.layer.cornerRadius = 10
-            brightnessButton.layer.masksToBounds = true
-            currentLocationButton.layer.cornerRadius = currentLocationButton.frame.size.width / 2
-            currentLocationButton.clipsToBounds = true
             
-     
+ 
             
             
     //SET TOOLBAR
@@ -111,31 +123,50 @@ class SearchByFiltersViewController:  UIViewController, UIPickerViewDelegate, UI
             let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
             let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.doneClicked))
             
-            let cancelButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.cancel, target: self, action: #selector(self.cancelClicked))
             
            
-            toolbar.setItems([cancelButton, flexibleSpace, doneButton], animated: false)
+            toolbar.setItems([flexibleSpace, doneButton], animated: false)
             
-            categoryTextField.inputAccessoryView = toolbar
-            spaceTextField.inputAccessoryView = toolbar
+            climateTextField.inputAccessoryView = toolbar
+            climatePickerView.dataSource = self
+            climatePickerView.delegate = self
+            climateTextField.tintColor = UIColor.clear
+            climateTextField.inputView = climatePickerView
+            
+            
+            brightnessTextField.inputAccessoryView = toolbar
+            brightnessPickerView.dataSource = self
+            brightnessPickerView.delegate = self
+            brightnessTextField.tintColor = UIColor.clear
+            brightnessTextField.inputView = brightnessPickerView
+            
+            
             dedicationTextField.inputAccessoryView = toolbar
-            
-            categoryPickerView.dataSource = self
-            categoryPickerView.delegate = self
-            
-            spacePickerView.dataSource = self
-            spacePickerView.delegate = self
-            
             dedicationPickerView.dataSource = self
             dedicationPickerView.delegate = self
-            
-            categoryTextField.tintColor = UIColor.clear
-            spaceTextField.tintColor = UIColor.clear
             dedicationTextField.tintColor = UIColor.clear
-            
-            categoryTextField.inputView = categoryPickerView
-            spaceTextField.inputView = spacePickerView
             dedicationTextField.inputView = dedicationPickerView
+            
+            
+            plantSizeTextField.inputAccessoryView = toolbar
+            plantSizePickerView.dataSource = self
+            plantSizePickerView.delegate = self
+            plantSizeTextField.tintColor = UIColor.clear
+            plantSizeTextField.inputView = plantSizePickerView
+            
+   
+            categoryTextField.inputAccessoryView = toolbar
+            categoryPickerView.dataSource = self
+            categoryPickerView.delegate = self
+            categoryTextField.tintColor = UIColor.clear
+            categoryTextField.inputView = categoryPickerView
+            
+            spaceTextField.inputAccessoryView = toolbar
+            spacePickerView.dataSource = self
+            spacePickerView.delegate = self
+            spaceTextField.tintColor = UIColor.clear
+            spaceTextField.inputView = spacePickerView
+            
             
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped(sender:)))
             view.addGestureRecognizer(tapGesture)
@@ -154,56 +185,34 @@ class SearchByFiltersViewController:  UIViewController, UIPickerViewDelegate, UI
     
     
         @objc func viewTapped(sender: UITapGestureRecognizer) {
+            climateTextField.resignFirstResponder()
+            brightnessTextField.resignFirstResponder()
+            dedicationTextField.resignFirstResponder()
+            plantSizeTextField.resignFirstResponder()
             categoryTextField.resignFirstResponder()
             spaceTextField.resignFirstResponder()
-            dedicationTextField.resignFirstResponder()
+            
+            
         }
     
         func textFieldDidBeginEditing(_ textField: UITextField) {
-            
-            if (textField == categoryTextField || textField == spaceTextField ){
-            scrollView.setContentOffset(CGPoint(x: 0, y: 220), animated: true)
+            if (textField == dedicationTextField || textField == plantSizeTextField ){
+            scrollView.setContentOffset(CGPoint(x: 0, y: 180), animated: true)
+            } else if (textField == categoryTextField || textField == spaceTextField ){
+                scrollView.setContentOffset(CGPoint(x: 0, y: 320), animated: true)
             }
         }
+    
         func textFieldDidEndEditing(_ textField: UITextField) {
-            if (textField == categoryTextField || textField == spaceTextField ){
+            if (textField == dedicationTextField || textField == plantSizeTextField ){
                 scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+            } else if (textField == categoryTextField || textField == spaceTextField ){
+                scrollView.setContentOffset(CGPoint(x: 0, y: 150), animated: true)
             }
         }
 
+
     
-    //BUTTON TO GET THE CURRENT LOCATION
-        
-        @IBAction func currentLocationButtonTapped(_ sender: Any) {
-            locationManager.startUpdatingLocation()
-            print("location: \(location)")
-            
-        }
-        
-        override func viewDidAppear(_ animated: Bool) {
-            locationManager = CLLocationManager()
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            checkCoreLocationPermission()
-        }
-        
-        
-        
-        func checkCoreLocationPermission(){
-            if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-                locationManager.startUpdatingLocation()
-            } else if CLLocationManager.authorizationStatus() == .notDetermined {
-                locationManager.requestWhenInUseAuthorization()
-            } else if CLLocationManager.authorizationStatus() == .restricted {
-                print("unauthorized location service")
-            }
-        }
-    
-        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [AnyObject]) {
-            location = (locations as! [CLLocation]).last
-            locationManager.stopUpdatingLocation()
-        }
-        
         
     //PICKER
     
@@ -212,67 +221,74 @@ class SearchByFiltersViewController:  UIViewController, UIPickerViewDelegate, UI
             return 1
         }
         
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
- 
-        if pickerView == categoryPickerView {
-            return category.count
-        } else if pickerView == spacePickerView {
-            return space.count
-        } else {
-            return dedication.count
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+     
+            if pickerView == climatePickerView {
+                return climate.count
+            } else if pickerView == brightnessPickerView {
+                return brightness.count
+            } else if pickerView == dedicationPickerView {
+                return dedication.count
+            } else if pickerView == plantSizePickerView {
+                return plantSize.count
+            } else if pickerView == categoryPickerView {
+                return category.count
+            } else {
+                return space.count
+            }
+            
         }
         
-    }
-        
         func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-            if pickerView == categoryPickerView {
-                return category[row]
-            } else if pickerView == spacePickerView{
-                return space[row]
-            } else {
+            if pickerView == climatePickerView {
+                return climate[row]
+            } else if pickerView == brightnessPickerView {
+                return brightness[row]
+            } else if pickerView == dedicationPickerView {
                 return dedication[row]
+            } else if pickerView == plantSizePickerView {
+                return plantSize[row]
+            } else if pickerView == categoryPickerView {
+                return category[row]
+            } else {
+                return space[row]
             }
            
         }
     
         func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
             
-            
-            if pickerView == categoryPickerView {
-                categoryTextField.text = category[row]
-            } else if pickerView == spacePickerView{
-                spaceTextField.text = space[row]
-            } else {
+            if pickerView == climatePickerView {
+                climateTextField.text = climate[row]
+            } else if pickerView == brightnessPickerView {
+                brightnessTextField.text = brightness[row]
+            } else if pickerView == dedicationPickerView {
                 dedicationTextField.text = dedication[row]
+            } else if pickerView == plantSizePickerView {
+                plantSizeTextField.text = plantSize[row]
+            } else if pickerView == categoryPickerView {
+                categoryTextField.text = category[row]
+            } else {
+                return spaceTextField.text = space[row]
             }
-          
-            scrollView.isScrollEnabled = false
+           
+
         }
     
     
         @objc func doneClicked() {
+            climateTextField.resignFirstResponder()
+            brightnessTextField.resignFirstResponder()
+            dedicationTextField.resignFirstResponder()
+            plantSizeTextField.resignFirstResponder()
             categoryTextField.resignFirstResponder()
             spaceTextField.resignFirstResponder()
-            dedicationTextField.resignFirstResponder()
-            view.endEditing(true)
-        }
-    
-        @objc func cancelClicked() {
-            view.endEditing(true)
-        }
-
- 
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
             
+            view.endEditing(true)
         }
     
-//        func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange SelectedScope: Int) -> Bool {
-//            return true
-//        }
-    
-        private func setUpSearchBar(){
-            searchBar.delegate = self
-        }
+       
+
 }
 
 
