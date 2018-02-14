5564476b34bac8f0b8c1628fc8fa0b8a8a16d72b
Luis Mautone <luismauto@hotmail.com>
2018-02-14 17:58:07 +0100

    Added functions in AppDelegate about JSON data upload (but commented), plants.json and images directory examples, CoreDataClasses directory with NSManagedObject classes and Core Data entities and relationships.

---
+++ Plant+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Plant+CoreDataClass.swift
+//  The Drifters
+//
+//  Created by Luis Mautone on 14/02/18.
+//  Copyright © 2018 Graziella Caputo. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Plant)
+public class Plant: NSManagedObject {
+
+}
