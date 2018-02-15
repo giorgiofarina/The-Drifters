67654195d241a8f7f5c7bfa202a9a85e0eb542a8
Graziella Caputo <caputograziella7@gmail.com>
2018-02-15 20:54:34 +0100

    fixed scrollview constraint

--- The Drifters/TabBarMainViewController.swift
+++ The Drifters/TabBarMainViewController.swift
@@ -13,11 +13,11 @@ class TabBarMainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let unselectedColor   = UIColor(red: 161, green: 39, blue: 23, alpha: 1.0)
-        let selectedColor = UIColor(red: 161, green: 39, blue: 23, alpha: 1.0)
+        let unselectedColor   = UIColor(red: 1.42, green: 1.42, blue: 1.47, alpha: 1.0)
+        let selectedColor = UIColor(red: 1.55, green: 0.19, blue: 0.00, alpha: 1.0)
         
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor:unselectedColor], for: .normal)
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor:selectedColor], for: .selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor: unselectedColor], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor: selectedColor], for: .selected)
         
         // Do any additional setup after loading the view.
     }
