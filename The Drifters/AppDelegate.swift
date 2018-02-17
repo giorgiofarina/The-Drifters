e8905ae636b77252b8e7b7f32ac7db31a0f9997b
Lucia Cotrozzi <luciacotrozzi@mbp-di-lucia.station>
2018-02-17 18:24:21 +0100

    Commented app delegate function

--- The Drifters/AppDelegate.swift
+++ The Drifters/AppDelegate.swift
@@ -20,16 +20,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
       
-        let defaults = UserDefaults.standard
-        if defaults.object(forKey: "isFirstTime") == nil {
-            defaults.set("No", forKey:"isFirstTime")
-            defaults.synchronize()
-            let storyboard = UIStoryboard(name: "OnBoarding", bundle: nil) //Write your storyboard name
-            let viewController = storyboard.instantiateViewController(withIdentifier: "Welcome") as! TabBarMainViewController
-            self.window?.rootViewController = viewController
-            self.window?.makeKeyAndVisible()
-        }
-     
+//        let defaults = UserDefaults.standard
+//        if defaults.object(forKey: "isFirstTime") == nil {
+//            defaults.set("No", forKey:"isFirstTime")
+//            defaults.synchronize()
+//            let storyboard = UIStoryboard(name: "OnBoarding", bundle: nil) //Write your storyboard name
+//            let viewController = storyboard.instantiateViewController(withIdentifier: "Welcome") as! TabBarMainViewController
+//            self.window?.rootViewController = viewController
+//            self.window?.makeKeyAndVisible()
+//        }
+//
         
         return true
     }
