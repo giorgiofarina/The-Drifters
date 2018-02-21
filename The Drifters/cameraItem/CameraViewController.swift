03e18f136d3767d3d91b802bdf10ecf0ab69596f
Graziella Caputo <caputograziella7@gmail.com>
2018-02-21 23:32:11 +0100

    Alert and font title updated on camera

--- The Drifters/cameraItem/CameraViewController.swift
+++ The Drifters/cameraItem/CameraViewController.swift
@@ -26,9 +26,6 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         let label2 = UILabel(frame: CGRect(x: 0, y: 0, width: 270, height: 37))
         label2.center = CGPoint(x: 150, y: 50)
         label2.textColor = UIColor(red: 155.0/255.0, green: 19.0/255.0, blue: 0.0/255.0, alpha: 1.0)
-//        label2.lineBreakMode = .byWordWrapping
-//        label2.numberOfLines = 2
-
         label2.font = UIFont.boldSystemFont(ofSize: 20.0)
         label2.textAlignment = .center
         label2.text = " "
@@ -49,17 +46,13 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         button2.clipsToBounds = true
         return button2
     }()
-
+    
     var list : [Plant] = []
     
     @IBOutlet weak var CameraV: UIView!
     
     @objc func bottone2 (){
-        let wishList = ritornaLista(nomeLista: "Wishlist")
-        aggiungiPianta(istanzaPianta: pianta[0], istanzaLista: wishList)
-        print("pianta aggiunta ai preferiti")
-        self.list = mostraLista(istanzaLista: wishList)
-        print("\(self.list[0])")
+        
     }
     
     
@@ -68,12 +61,9 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         
         super.viewDidLoad()
         
-      
         
-        self.view.addSubview(label2)
-        self.view.addSubview(button2)
-        
-        button2.addTarget(self, action:#selector(self.bottone2), for: .touchUpInside)
+        //        self.view.addSubview(button2)
+        //        button2.addTarget(self, action:#selector(self.bottone2), for: .touchUpInside)
         
         //Initialize session an output variables this is necessary
         let session = AVCaptureSession()
@@ -110,10 +100,8 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
     
     
 //    //        per coreml
+//
 //    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-//
-//
-//
 //
 //
 //        let pixelBuffer : CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
@@ -121,37 +109,95 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
 //        let model = try? VNCoreMLModel(for: Oxford102().model)
 //        let request = VNCoreMLRequest(model: model!){ (finishedReq, err) in
 //
-//            //            print(finishedReq.results)
 //            guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
 //            guard let firstObservation = results.first else {return}
 //
 //            DispatchQueue.main.async(execute: {
 //
-//                self.label2.text = "\(firstObservation.identifier) \(firstObservation.confidence)"
-//                self.label2.backgroundColor = .white
+//                self.label2.text = firstObservation.identifier
 //
-////                controllo tra nome del modello al nome dal database
-//               aggiungiFiltri(nomeFiltro: "commonName", valoreFiltro: firstObservation.identifier)
+//
+//                //controllo tra nome del modello al nome dal database
+//
+//                aggiungiFiltri(nomeFiltro: "commonName", valoreFiltro: firstObservation.identifier)
 //                self.pianta = ricercaPerFiltri(arrayFiltri: filtri)
-//               svuotaFiltri()
-//                if(self.pianta.count != 0){
-//                    self.label2.text = "\(String(describing: self.pianta[0].generalDescription!))"
+//                svuotaFiltri()
 //
-//                    self.label2.backgroundColor = .white
+//                if(self.pianta.count != 0){
+//                    //                    self.Alert(Title: "\(self.label2.text!)")
+//                    self.showActionSheet(Title: "\(self.label2.text!)")
+//                    self.tabBarController?.tabBar.isHidden = true
+//
 //                }
 //                else{
-//                    self.label2.text = " "
-//                    self.label2.backgroundColor = .clear
+//
 //                }
 //            })
 //
 //
 //            print(firstObservation.identifier, firstObservation.confidence)
-//            sleep(3)
+//            sleep(2)
 //        }
 //        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
 //    }
-
+    
+    func showActionSheet(Title: String){
+        
+        let titleAlert = NSAttributedString(string: Title, attributes: [NSAttributedStringKey.font : UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.semibold) , NSAttributedStringKey.foregroundColor: UIColor.gray])
+        
+        let actionSheet = UIAlertController(title: Title, message: nil, preferredStyle: .actionSheet)
+        actionSheet.setValue(titleAlert, forKey: "attributedTitle")
+        
+        actionSheet.view.tintColor = UIColor(red: 155.0/255.0, green: 19.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            self.tabBarController?.tabBar.isHidden = false
+            self.tabBarController?.tabBar.layer.zPosition = 0
+        }
+        
+        let add = UIAlertAction(title: "Add to Wishilist", style: .default) { action in
+            
+            let wishList = ritornaLista(nomeLista: "Wishlist")
+            aggiungiPianta(istanzaPianta: self.pianta[0], istanzaLista: wishList)
+            self.tabBarController?.tabBar.isHidden = false
+            
+            
+        }
+        actionSheet.addAction(add)
+        actionSheet.addAction(cancel)
+        
+        present(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    func Alert(Title: String) {
+        
+        //        let TitleString = NSAttributedString(string: Title, attributes: [NSAttributedStringKey.font : UIFont(name: "Helvetica-Bold", size: 15.0)!, NSAttributedStringKey.foregroundColor: UIColor.white])
+        
+        let TitleString = NSAttributedString(string: Title, attributes: [NSAttributedStringKey.font : UIFont.systemFont(ofSize: 34, weight: UIFont.Weight.heavy) , NSAttributedStringKey.foregroundColor: UIColor.white])
+        
+        let alertController = UIAlertController(title: Title, message: nil, preferredStyle: .alert)
+        
+        alertController.setValue(TitleString, forKey: "attributedTitle")
+        
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
     func getDevice(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
         let devices: NSArray = AVCaptureDevice.devices() as NSArray;
         for de in devices {
